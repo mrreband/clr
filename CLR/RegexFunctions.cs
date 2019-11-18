4068683a7a678c62a64c2931d456cc6aab8f0551
mrreband <mr@reband.org>
2019-11-18 09:13:58 -0600

    initial commit with 3 regex functions

---
+++ CLR/RegexFunctions.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.SqlTypes;
+using System.Text.RegularExpressions;
+using Microsoft.SqlServer.Server;
+
+public partial class UserDefinedFunctions
+{
+    [SqlFunction]
+    public static SqlString RegexReplace(SqlString TextString, SqlString RegexPattern, SqlString ReplaceString)
+    {
+        var textString = (TextString.IsNull) ? "" : TextString.ToString();
+        var regexPattern = (TextString.IsNull) ? "" : RegexPattern.ToString();
+        var replaceString = (ReplaceString.IsNull) ? "" : ReplaceString.ToString();
+
+        var regex = new Regex(regexPattern, RegexOptions.IgnoreCase);
+        var replacedText = regex.Replace(textString, replaceString);
+        return new SqlString(replacedText);
+    }
+
+    [SqlFunction]
+    public static bool RegexIsMatch(SqlString TextString, SqlString RegexPattern)
+    {
+        var textString = (TextString.IsNull) ? "" : TextString.ToString();
+        var regexPattern = (TextString.IsNull) ? "" : RegexPattern.ToString();
+
+        Regex r1 = new Regex(regexPattern.TrimEnd(null));
+        return r1.Match(textString.TrimEnd(null)).Success;
+    }
+
+    [SqlFunction]
+    public static SqlString RegexMatch(SqlString TextString, SqlString RegexPattern)
+    {
+        var textString = (TextString.IsNull) ? "" : TextString.ToString();
+        var regexPattern = (TextString.IsNull) ? "" : RegexPattern.ToString();
+
+        Regex r1 = new Regex(regexPattern.TrimEnd(null));
+        var match = r1.Match(textString.TrimEnd(null)).Value;
+        return (match == "") ? (SqlString)null : match;
+    }
+}
