b28f8254093a7d0ba7b43dbd143a4290c02da13e
mrreband <mr@reband.org>
2020-06-02 08:34:38 -0500

    adding a spoonerize method

--- CLR/dbo/CLR/StringFunctions.cs
+++ CLR/dbo/CLR/StringFunctions.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Data.SqlTypes;
 using Microsoft.SqlServer.Server;
 
@@ -32,5 +33,28 @@ public partial class UserDefinedFunctions
         var textString = (TextString.IsNull) ? "" : TextString.ToString();
         var alphas = new String(textString.Where(Char.IsLetter).ToArray());
         return alphas;
+    }
+
+    [SqlFunction]
+    public static SqlString Spoonerize(SqlString TextString)
+    {
+        var textString = (TextString.IsNull) ? "" : TextString.ToString();
+        var word1 = Regex.Replace(textString.Split(' ')[0].Trim(), @"[^\w]", "");
+        var word2 = Regex.Replace(textString.Split(' ')[1].Trim(), @"[^\w]", "");
+
+        var consonants1 = Regex.Match(word1, @"^[bcdfghjklmnpqrstvwxyz]+", RegexOptions.IgnoreCase);
+        var consonants2 = Regex.Match(word2, @"^[bcdfghjklmnpqrstvwxyz]+", RegexOptions.IgnoreCase);
+
+        var spoon1start = consonants2.Value;
+        var spoon1end = (consonants1.Success) ? word1.Substring(consonants1.Index + 1) : word1;
+
+        var spoon2start = (consonants1.Success) ? consonants1.Value : "";
+        var spoon2end = (consonants2.Success) ? word2.Substring(consonants2.Index + 1) : word2;
+
+        var spoon1 = spoon1start + spoon1end;
+        var spoon2 = spoon2start + spoon2end;
+
+        var spoonerized = spoon1 + " " + spoon2;
+        return spoonerized;
     }
 }
